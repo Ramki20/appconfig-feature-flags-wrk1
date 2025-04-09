297dc715d8aba2b6a5bb87179f1e57dfed91ac13
Sivaram.Nataraj <sivaram.nataraj@usda.gov>
2025-04-08 21:26:10 -0500

    appconfig feature flags wrk - initial commit

---
+++ terraform/outputs.tf
@@ -0,0 +1,29 @@
+output "application_id" {
+  description = "AWS AppConfig Application ID"
+  value       = aws_appconfig_application.feature_flags_app.id
+}
+
+output "environment_id" {
+  description = "AWS AppConfig Environment ID"
+  value       = aws_appconfig_environment.feature_flags_env.id
+}
+
+output "configuration_profile_id" {
+  description = "AWS AppConfig Configuration Profile ID"
+  value       = aws_appconfig_configuration_profile.feature_flags_profile.id
+}
+
+output "deployment_strategy_id" {
+  description = "AWS AppConfig Deployment Strategy ID"
+  value       = aws_appconfig_deployment_strategy.quick_deployment.id
+}
+
+output "deployment_id" {
+  description = "AWS AppConfig Deployment ID"
+  value       = aws_appconfig_deployment.feature_flags_deployment.id
+}
+
+output "deployment_status" {
+  description = "AWS AppConfig Deployment Status"
+  value       = aws_appconfig_deployment.feature_flags_deployment.state
+}
